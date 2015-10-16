316fd3a39b5a808d43de0798392a8995e6285889
unknown <cecilia@ATC312-C06.business-cs.deanza.ad.fhda.edu>
2015-10-16 16:00:04 -0700

    Added cookie.png and Charizard can now eat it :D

--- src/Overlap.h
+++ src/Overlap.h
@@ -2,7 +2,8 @@
 #define OVERLAP_H
 
 #include <SFML/Graphics.hpp>
-
+/*"bool" is overlab
+it turns true when it overlabs, if not then it turns false*/
 bool overlap(sf::Sprite& sprite1, sf::Sprite& sprite2);
 
 #endif
